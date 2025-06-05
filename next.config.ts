6078de4968dab9f699e381bbb68ee64cff7e0b59
Random Random <polemace1@gmail.com>
2025-06-05 03:41:36 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -18,6 +19,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  allowedDevOrigins: ['https://6000-firebase-studio-1748635166583.cluster-ombtxv25tbd6yrjpp3lukp6zhc.cloudworkstations.dev'],
 };
 
 export default nextConfig;
